11063141f897088ea3a9c03252e6993178dd94c4
David Poetzsch-Heffer <dpoetzsch@posteo.de>
2015-07-16 17:47:18 -0700

    added example for struct padding

---
+++ padded-struct/padded-struct.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct padded_struct {
+  char ch1; // 1 byte
+  // 3 padding bytes
+  int i1;   // 4 bytes on 64bits system
+  int i2;   // 4 bytes
+};
+
+int main(int argc, char *argv[]) {
+  struct padded_struct foo;
+    
+  foo.ch1 = 'a';
+  foo.i1 = 0;
+  foo.i2 = 1;
+    
+  printf("sizeof padded_struct is %ld\n", sizeof(foo)); 
+  /* sizeof padded_struct is 12 */
+    
+  write(2, &foo, sizeof(foo));
+  /* Argument mismatch triggered if ASLR is enabled */
+  return 0;
+}
