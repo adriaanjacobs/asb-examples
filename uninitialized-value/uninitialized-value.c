f269c8ceb69fd56bbe8826f1a4ba899ead1e8a9e
thomas <croustibaie.bourguenolle@ensta.fr>
2015-07-17 10:11:49 -0700

    fixed uninitialized variable problem

--- uninitialized-value/uninitialized-value.c
+++ uninitialized-value/uninitialized-value.c
@@ -5,14 +5,17 @@
  * both, gcc and clang will find this problem.
  */
 
+int* print_uninit()
+{
+  int x[10];
+  printf("%p\n", x);
+  printf("%d\n", x[4]);
+  return &x;
+}
+
 int main(int argc, char** argv) {
-  int x;
-  int rndseed;
-  int a;
 
-  srand(rndseed);
-  printf("%d\n", rndseed);
-  printf("%d\n", rand());
-
+  int *p=print_uninit();
+  printf("the value is %d\n",p[4]);
   return 0;
 }
