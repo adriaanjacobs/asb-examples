f0c4493fe141b0b2879557734445825c87b2aea5
David Poetzsch-Heffer <dpoetzsch@posteo.de>
2015-08-12 16:01:16 -0700

    added debug info (-g) to build

--- hatasi-hashing/g++-compilation.sh
+++ hatasi-hashing/g++-compilation.sh
@@ -1,4 +1,4 @@
 #/bin/sh
 
 mkdir -p bin
-g++ -I$HOME/valgrind-3.10.1/taintgrind/ -I$HOME/valgrind-3.10.1/include/ -Wall sources/*.cpp -o bin/hatasi-hashing
+g++ -I$HOME/valgrind-3.10.1/taintgrind/ -I$HOME/valgrind-3.10.1/include/ -g -O0 -Wall sources/*.cpp -o bin/hatasi-hashing
