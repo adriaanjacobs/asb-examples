1c8538382ff4891744950ee445f44bd440b89f1c
Adriaan Jacobs <adriaan.jacobs@student.kuleuven.be>
2021-03-01 16:29:47 +0100

    updated padded-struct buildsystem

--- padded-struct/gcc-compilation-script-optimizationOFF.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-mkdir -p bin
-gcc -Wall -O0 sources/padded-struct.c -o bin/padded-struct
--- padded-struct/gcc-compilation-script-optimizationON.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-mkdir -p bin
-gcc -Wall -O1 sources/padded-struct.c -o bin/padded-struct
