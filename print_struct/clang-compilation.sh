0a67947e09becf49a6cd1c80e1e13366114123e1
Adriaan Jacobs <adriaan.jacobs@student.kuleuven.be>
2021-03-01 16:31:39 +0100

    updated print_struct build system

--- print_struct/clang-compilation.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-mkdir -p bin
-clang -fsanitize=memory -Wall  sources/print_struct.c -o bin/print_struct
--- print_struct/gcc-compilation-script-optimizationON.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-mkdir -p bin
-gcc -Wall -O1 sources/print_struct.c -o bin/print_struct
