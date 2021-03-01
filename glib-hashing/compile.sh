a72cb65e7f3368a15b2ea980ab887e2f103fa48d
Adriaan Jacobs <adriaan.jacobs@student.kuleuven.be>
2021-03-01 16:21:17 +0100

    updated build system for glib-hashing

--- glib-hashing/compile.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-GLIB_HOME=$HOME/src/glib2.0-2.40.2/install
-
-gcc glib-hashing.c -I$GLIB_HOME/include/glib-2.0 -I$GLIB_HOME/lib/glib-2.0/include/ -lglib-2.0
--- glib-hashing/run-taint-analyses.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-
-exec ~/valgrind-3.10.1/inst/bin/valgrind --tool=taintgrind --critical-ins-only=no ./a.out 2>&1 | ~/asb-llvm-plugins/process-taintgrind-output.rb
