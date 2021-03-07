3dc7b0d0153aab85c69aee33e4ba20f9a5a9edb5
Adriaan Jacobs <adriaan.jacobs@student.kuleuven.be>
2021-03-07 23:30:13 +0100

    increased memory efficiency in theoretical metallocator

--- unification/unify_allocator_impl.cpp
+++ unification/unify_allocator_impl.cpp
@@ -4,71 +4,103 @@
 #include <cstdint>
 #include <algorithm>
 #include <assert.h>
-#include <set>
 
-struct unified_entry {
-    uintptr_t address;
-    size_t size; // will not need this eventually
-};
+static_assert(sizeof(char) == 1);
 
 struct alloc_entry {
-    unified_entry unif; 
     void* allocation;
+    size_t size; 
 
-    bool operator < (const alloc_entry& other) const {
-        return unif.address < other.unif.address;
+    bool operator == (void* other) const {
+        return allocation == other;
     }
 };
 
-static std::set<alloc_entry> alloc_list;
-static std::vector<unified_entry> free_list;
+// again, SOA thing
+static std::vector<alloc_entry> alloc_list;
+static std::vector<bool> free_list;
 
 extern "C" {
 
 void* register_alloc(void* ptr, size_t bytes) {
-    auto free = std::find_if(free_list.begin(), free_list.end(), [bytes](const auto& entry) -> bool {
-        return entry.size >= bytes;
-    });
-    if (free != free_list.end()) {
-        if (bytes == free->size) {
-            alloc_list.insert({
-                .unif = *free,
-                .allocation = ptr
-            });
-            free_list.erase(free);
-        } else {
-            assert(free->size > bytes);
-            alloc_list.insert({
-                .unif = {
-                    .address = free->address + bytes,
-                    .size = free->size - bytes
-                },
-                .allocation = ptr,
-            });
-            free->size -= bytes; // if I ever sort the free_list by size, I should reinsert here
-            static_assert(std::is_same<decltype(free_list), std::vector<unified_entry>>::value); 
-        }
-    } else {
-        alloc_list.insert(alloc_list.end(), {
-            .unif = {
-                .address = alloc_list.empty() ? 0 : alloc_list.rbegin()->unif.address + alloc_list.rbegin()->unif.size,
-                .size = bytes
-            },
-            .allocation = ptr
-        });
-        if (alloc_list.size() >= 2) 
-            assert(alloc_list.rbegin()->unif.address > alloc_list.begin()->unif.address);
+
+    { // DEBUG
+        auto again = std::find(alloc_list.begin(), alloc_list.end(), ptr);
+        assert(again == alloc_list.end());
     }
+
+    // first fit
+    bool done = false;
+    for (size_t idx = 0; idx < free_list.size() && !done; idx++) {
+        if ((done = (free_list[idx] && alloc_list[idx].size >= bytes))) {
+            free_list[idx] = false;
+            alloc_list[idx].allocation = ptr;
+            if (alloc_list[idx].size > bytes) {
+                alloc_entry new_alloc = { .allocation = nullptr, .size = alloc_list[idx].size - bytes };
+                alloc_list[idx].size = bytes;
+                alloc_list.insert(alloc_list.begin() + idx + 1, new_alloc);
+                free_list.insert(free_list.begin() + idx + 1, true);
+            }
+        }
+    }
+
+    if (!done) { // no suitable free block available
+        alloc_list.push_back({ .allocation = ptr, .size = bytes });
+        free_list.push_back(false);
+    }
+
+    assert(free_list.size() == alloc_list.size());
     return ptr;
 }
 
 void* unregister_alloc(void* ptr) {
-    auto entry = std::find_if(alloc_list.begin(), alloc_list.end(), [ptr](const auto& other) -> bool {
-        return other.allocation == ptr;
-    });
-    assert(entry != alloc_list.end()); // crash signifies double or invalid free
-    free_list.push_back(entry->unif);
+    { // iterator invalidation protection
+        auto entry = std::find(alloc_list.begin(), alloc_list.end(), ptr);
+        assert(entry != alloc_list.end());
+        auto free = free_list.begin() + std::distance(alloc_list.begin(), entry);
+        *free = true;
+
+        // coalesc blocks
+        auto leftmost = (free != free_list.begin() && *(free - 1)) ? entry - 1 : entry;
+        auto rightmost = (((free != free_list.end() - 1) && *(free + 1)) ? entry + 1 : entry) + 1;
+        for (auto it = leftmost + 1; it != rightmost; it++) {
+            leftmost->size += it->size;
+        }
+        free_list.erase(
+            free_list.begin() + std::distance(alloc_list.begin(), leftmost) + 1, 
+            free_list.begin() + std::distance(alloc_list.begin(), rightmost)
+        );
+        alloc_list.erase(leftmost + 1, rightmost);
+    }
+
+    if (*free_list.rbegin()) {
+        free_list.pop_back();
+        alloc_list.pop_back();
+    }
+
+    assert(free_list.size() == alloc_list.size());
+    assert(!*free_list.rbegin());
     return ptr;
+}
+
+uintptr_t unify(void* v_addr) {
+    for (uintptr_t idx = 0, occ_idx = 0; idx < alloc_list.size(); idx++, occ_idx += alloc_list[idx].size) {
+        if (v_addr >= alloc_list[idx].allocation && v_addr < (static_cast<char*>(alloc_list[idx].allocation) + alloc_list[idx].size))
+            return occ_idx + (static_cast<char*>(v_addr) - static_cast<char*>(alloc_list[idx].allocation));
+    }
+
+    assert(false && "v_addr was not contained in any of the alloc_entries!");
+    return 0;
+}
+
+void* deunify(uintptr_t u_addr) {
+    for (uintptr_t idx = 0, occ_idx = 0; idx < alloc_list.size(); idx++, occ_idx += alloc_list[idx].size) {
+        if (u_addr >= occ_idx && u_addr < occ_idx + alloc_list[idx].size)
+            return static_cast<char*>(alloc_list[idx].allocation) + (u_addr - occ_idx);
+    }
+
+    assert(false && "u_addr was not contained in any of the alloc_entries!");
+    return 0;
 }
 
 }
